0cab6766d5cf9fb2f162ef2160fabfe8ef38d2bc
Abhishek Kumar <Abhishekk55@birlasoft.com>
2024-06-05 12:10:11 +0530

    fix: class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+interface IUser {
+  id: number,
+  firstName: string,
+  lastName: string,
+  gender: IGender,
+  isActive: boolean
+}
+
+enum IGender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  OTHER = 'Other'
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appdemo';
+
+  user: IUser = {
+    id: 101,
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: IGender.MALE,
+    isActive: true
+  } 
+
+  
 }
